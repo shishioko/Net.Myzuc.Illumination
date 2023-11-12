6c86328fce6c28a35f477d9ba8eed777f51227b1
0x3183 <depecksti@gmail.com>
2023-11-12 13:19:50 +0100

    Merged ResolvedScore into ChatScore.

--- Net.Myzuc.Illumination/Chat/ChatScore.cs
+++ Net.Myzuc.Illumination/Chat/ChatScore.cs
@@ -4,6 +4,21 @@ namespace Net.Myzuc.Illumination.Chat
 {
     public sealed class ChatScore : ChatComponent
     {
+        public sealed class ResolvedScore
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("objective")]
+            public string Objective { get; set; }
+            [JsonProperty("value")]
+            public int Value { get; set; }
+            public ResolvedScore(string name, string objective, int value)
+            {
+                Name = name;
+                Objective = objective;
+                Value = value;
+            }
+        }
 
         [JsonProperty("score")]
         public ResolvedScore Score { get; set; }
