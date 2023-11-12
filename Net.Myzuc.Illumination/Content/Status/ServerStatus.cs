d301aeac654b3a22264f25069561759defb4f44c
0x3183 <depecksti@gmail.com>
2023-11-12 03:46:57 +0100

    Merged status related classes into 1 file.

--- Net.Myzuc.Illumination/Content/Status/ServerStatus.cs
+++ Net.Myzuc.Illumination/Content/Status/ServerStatus.cs
@@ -1,10 +1,51 @@
 ﻿using Net.Myzuc.Illumination.Chat;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace Net.Myzuc.Illumination.Content.Status
 {
     public sealed class ServerStatus
     {
+        public sealed class StatusPlayerInfo
+        {
+            [JsonProperty("max")]
+            public int? Max { get; set; }
+            [JsonProperty("online")]
+            public int? Online { get; set; }
+            [JsonProperty("sample")]
+            public IEnumerable<StatusPlayer>? Sample { get; set; }
+            public StatusPlayerInfo()
+            {
+                Max = null;
+                Online = null;
+                Sample = null;
+            }
+        }
+        public sealed class StatusPlayer
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("id")]
+            public Guid Id { get; set; }
+            public StatusPlayer(string name, Guid id)
+            {
+                Name = name;
+                Id = id;
+            }
+        }
+        public sealed class ServerVersion
+        {
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("protocol")]
+            public int Protocol { get; set; }
+            public ServerVersion(string name, int protocol)
+            {
+                Name = name;
+                Protocol = protocol;
+            }
+        }
         [JsonProperty("version")]
         public ServerVersion? Version { get; set; }
         [JsonProperty("players")]
